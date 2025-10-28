391e1517d03cfeb9c33029a8d33d22ed0a668806
RHThree <russelheiser@gmail.com>
2025-10-27 22:45:30 -0400

    ALUMNI - student testimonials

--- src/app/alumni/page.tsx
+++ src/app/alumni/page.tsx
@@ -1,6 +1,7 @@
 import Companies from "@/components/alumni/Companies";
 import OurIBEAlumni from "@/components/alumni/OurIBEAlumni";
 import AlumniSpotlights from "@/components/alumni/AlumniSpotlights";
+import StudentTestimonials from "@/components/alumni/StudentTestimonials";
 
 export default function About() {
   return (
@@ -8,6 +9,7 @@ export default function About() {
       <OurIBEAlumni />
       <Companies />
       <AlumniSpotlights />
+      <StudentTestimonials />
     </>
   );
 }
