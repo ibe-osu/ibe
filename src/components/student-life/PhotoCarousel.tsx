7d44fcbf6a8a30b2699426c4a221ba79e4931cb5
James Butts <jamespbutts3@gmail.com>
2025-11-02 16:08:23 -0500

    adjustment

--- src/components/student-life/PhotoCarousel.tsx
+++ src/components/student-life/PhotoCarousel.tsx
@@ -84,7 +84,7 @@ export default function PhotoCarousel(props: IProps) {
             key={i}
             sx={{
               minWidth: "100%",
-              aspectRatio: "16 / 10",
+              aspectRatio: "16 / 9",
               position: "relative",
               backgroundColor: "grey.300",
             }}
